fe1013d3f7be83fefb003bfb030b5458d641a9b9
jnlou <131515346+jnlou@users.noreply.github.com>
2024-02-12 10:24:54 -0500

    Add files via upload

---
+++ P1LAB1_Louis-PierreJeremiah.py
@@ -0,0 +1,8 @@
+# Jeremiah Louis-Pierre
+# 2/12/2024
+# P1LAB1
+# This project takes a user's name as input and prints a welcome message, using the user's name
+
+name = input("Enter your name: ").title().strip()
+print(f"Hey {name}")
+print(f'Welcome to zyBooks!')
