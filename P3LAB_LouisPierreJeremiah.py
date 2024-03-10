79791f5d234912dd9e7efd900fd04c4e7fce82dc
jnlou <131515346+jnlou@users.noreply.github.com>
2024-03-10 10:42:38 -0400

    Add files via upload

---
+++ P3LAB_LouisPierreJeremiah.py
@@ -0,0 +1,20 @@
+# Jeremiah Louis-Pierre
+# 3/1/2024
+# P3LAB
+# This program determines if a year is a leap year.
+# This is determined based on two conditions:
+#   1) The year must be divisible by 4.
+#   2) If the year is a century year, the year must be evenly divisible by 400.
+
+input_year = int(input().strip())
+# turned the number input into a string for the purpose of referring to the index of its ending numbers
+if str(input_year)[-2:] == '00':
+    if input_year % 400 != 0:
+        print(f"{input_year} - not a leap year")
+    else:
+        print(f"{input_year} - leap year")
+else:
+    if input_year % 4 != 0:
+        print(f"{input_year} - not a leap year")
+    else:
+        print(f"{input_year} - leap year")
